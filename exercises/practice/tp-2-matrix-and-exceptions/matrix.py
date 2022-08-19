4c7bb82ee710dd6cbd09f318d5778838f7e18512
Juan Longo <juan.longo@mulesoft.com>
2022-08-19 01:26:26 -0300

    Making tp 2 more complicated

--- exercises/practice/tp-2-matrix-and-exceptions/matrix.py
+++ exercises/practice/tp-2-matrix-and-exceptions/matrix.py
@@ -7,12 +7,3 @@ class Matrix:
 
     def __init__(self, matrix: List[List[float]]) -> None:
         self.matrix = matrix
-
-    def __mul__(self, m: Matrix | int) -> Matrix:
-        return self
-
-    def __add__(self, m: Matrix) -> Matrix:
-        return self
-
-    def __invert__(self) -> Matrix:
-        pass
