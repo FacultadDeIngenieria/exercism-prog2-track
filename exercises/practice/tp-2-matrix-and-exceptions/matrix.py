2e7e0ef673580980d4ff0f3814281f3c4ed62a5b
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2023-03-06 19:33:22 -0300

    stripped repo

--- exercises/practice/tp-2-matrix-and-exceptions/matrix.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from typing import List
-
-
-class Matrix:
-
-    def __init__(self, matrix: List[List[float]]) -> None:
-        self.matrix = matrix
--- exercises/practice/tp-7-trapezoid-rule/trapezoid_rule.py
+++
@@ -1,5 +0,0 @@
-from typing import Callable
-
-
-def integrate(f: Callable[[float], float], a: float, b: float, n: int) -> float:
-    pass
