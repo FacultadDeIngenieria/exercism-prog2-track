ea0f3ae5ba19de1399367bc242d7a03f723211ff
Matías Ponce <matias.ponce@mulesoft.com>
2023-03-16 18:39:33 -0300

    Fixed Installing locally test

--- exercises/practice/installing-locally/installing_locally_tests.py
+++ exercises/practice/installing-locally/installing_locally_tests.py
@@ -1,5 +1,15 @@
 import unittest
 
-class LocalInstallationTest(unittest.TestCase):
-    def test(self):
-        pass
+from installing_locally import (
+    hello,
+)
+
+# Tests adapted from `problem-specifications//canonical-data.json`
+
+class InstallingLocallyTest(unittest.TestCase):
+    def test_say_hi(self):
+        self.assertEqual(hello(), "Hello, World!")
+
+
+if __name__ == "__main__":
+    unittest.main()
