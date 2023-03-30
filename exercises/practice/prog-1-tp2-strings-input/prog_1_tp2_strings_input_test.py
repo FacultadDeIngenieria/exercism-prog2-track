e2e61509d751d9582eb7d31e6cd7089fb710e671
Matías Ponce <matias.ponce@mulesoft.com>
2023-03-29 21:32:51 -0300

    Improved String input test

--- exercises/practice/prog-1-tp2-strings-input/prog_1_tp2_strings_input_test.py
+++ exercises/practice/prog-1-tp2-strings-input/prog_1_tp2_strings_input_test.py
@@ -1,10 +1,27 @@
 import unittest
+import io
+import sys
 
 
 class StringInputTest(unittest.TestCase):
     def testString(self):
-        pass
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
 
+        import prog_1_tp2_strings_input
+
+        strip = captured_output.getvalue().strip()
+        self.assertEqual(strip, '''Ingresar gasto:
+23.75
+Dinero recibido
+100
+
+Vuelto
+
+Pesos:
+76
+Centavos:
+25''')
 
 if __name__ == "__main__":
     unittest.main()
