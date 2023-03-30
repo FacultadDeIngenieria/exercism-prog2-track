5be979a8fa777defb3e780aa4b13f8e2715080e2
Matías Ponce <matias.ponce@mulesoft.com>
2023-03-29 21:20:36 -0300

    Improved String concatenation test

--- exercises/practice/prog-1-tp2-strings-concatenation/prog_1_tp2_strings_concatenation_test.py
+++ exercises/practice/prog-1-tp2-strings-concatenation/prog_1_tp2_strings_concatenation_test.py
@@ -1,9 +1,17 @@
 import unittest
+import io
+import sys
 
 
 class StringConcatenationTest(unittest.TestCase):
     def testString(self):
-        pass
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        import prog_1_tp2_strings_concatenation
+
+        strip = captured_output.getvalue().strip()
+        self.assertEqual(strip, '''The result of Bangladesh comes first in the dictionary than Barbados is True.''')
 
 
 if __name__ == "__main__":
