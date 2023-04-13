e33330bc3e3f42c1511b8844ec2b394e6bbf3e4b
Matías Ponce <matias.ponce@mulesoft.com>
2023-04-13 19:47:57 -0300

    Added tp3 line equation
    (cherry picked from commit 4c2043eda80da32826b5afb79aa07607c028a839)

---
+++ exercises/practice/prog-1-tp3-line/prog_1_tp3_line_equation_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+# Tests adapted from `problem-specifications//canonical-data.json`
+
+
+class LineEquationTest(unittest.TestCase):
+    def test_equation(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
