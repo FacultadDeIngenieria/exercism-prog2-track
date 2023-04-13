61a8e24167a23851cd028d599f47bb2667f006c8
Matías Ponce <matias.ponce@mulesoft.com>
2023-04-13 19:52:45 -0300

    Added tp3 leap year

---
+++ exercises/practice/prog-1-tp3-leap-year/prog_1_tp3_leap_year_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+# Tests adapted from `problem-specifications//canonical-data.json`
+
+
+class LeapYearTest(unittest.TestCase):
+    def test_leap_year(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
