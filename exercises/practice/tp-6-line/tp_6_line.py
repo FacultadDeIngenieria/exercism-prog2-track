28a214daf9e4e6ae08591a1a4a54d836f6f8caaa
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2023-04-13 23:23:26 -0300

    added tp6 line

---
+++ exercises/practice/tp-6-line/tp_6_line.py
@@ -0,0 +1,24 @@
+# a = input("Ingrese coeficiente A")
+# b = input("Ingrese coeficiente B")
+# x1 = input("Ingrese coeficiente X1")
+# x2 = input("Ingrese coeficiente X2")
+
+
+def equation_to_string(a, b):
+    return ""
+
+def point_to_string(a, b, x):
+    return ""
+
+def distance(a, b, x1, x2):
+    return 0
+
+
+# println("Para la siguiente ecuacion: ")
+# println(equation_to_string(a, b))
+
+# println("Dados los siguientes puntos:")
+# println("P1 " + point_to_string(a, b, x1))
+# println("P2 " + point_to_string(a, b, x2))
+
+println("La distancia entre ellos es: " + distance(a, b, x1, x2)
---
+++ exercises/practice/tp-6-line/tp_6_line_test.py
@@ -0,0 +1,29 @@
+import unittest
+
+# Tests adapted from `problem-specifications//canonical-data.json`
+
+from tp_6_line.py import (
+    equation_to_string,
+    point_to_string,
+    distance
+)
+
+class LineEquationTest(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(equation_to_string(1, 2), "Y = 1x  + 2")
+
+    def test_2(self):
+        self.assertEqual(equation_to_string(-1, -2), "Y = -1x  + -2")
+
+    def test_3(self):
+        self.assertEqual(point_to_string(1, 1, 0), "(0, 1)")
+
+    def test_4(self):
+        self.assertEqual(point_to_string(1, 1, 1), "(0, 2)")
+
+    def test_5(self):
+        self.assertEqual(distance(1, 1, 0, 3), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
