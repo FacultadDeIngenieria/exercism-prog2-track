00434aa46b81954d6070fc5dbeab52d1a3566495
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2023-04-13 22:36:16 -0300

    testing

--- exercises/practice/tp-3-formatting-names/tp_3_formatting_names.py
+++ exercises/practice/tp-3-formatting-names/tp_3_formatting_names.py
@@ -1,2 +1,14 @@
 first_name = AdA
 last_name  = LoVeLAce
+
+def all_lower_case():
+    return ""
+
+def initial_as_upper_case():
+    return ""
+
+def all_upper_case():
+    return ""
+
+def tabbed():
+    return ""
--- exercises/practice/tp-3-formatting-names/tp_3_formatting_names_test.py
+++ exercises/practice/tp-3-formatting-names/tp_3_formatting_names_test.py
@@ -2,10 +2,22 @@ import unittest
 
 # Tests adapted from `problem-specifications//canonical-data.json`
 
+from tp_3_formatting_names import (
+    all_lower_case,
+    initial_as_upper_case,
+    all_upper_case,
+    tabbed
+)
 
 class HelloWorldTest(unittest.TestCase):
-    def test_say_hi(self):
-        self.assertEqual("Hello, World!", "Hello, World!")
+    def test_1(self):
+        self.assertEqual(all_lower_case(), "ada lovelace")
+    def test_2(self):
+        self.assertEqual(initial_as_upper_case(), "Ada Lovelace")
+    def test_3(self):
+        self.assertEqual(all_upper_case(), "ADA LOVELACE")
+    def test_4(self):
+        self.assertEqual(tabbed(), "    ada lovelace")
 
 
 if __name__ == "__main__":
