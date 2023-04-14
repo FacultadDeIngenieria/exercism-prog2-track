7c8e6e1f910f9bcdc66b583c0973bb316ffbdb68
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2023-04-13 22:30:00 -0300

    changes

--- exercises/practice/tp-3-formatting-names/tp_3_formatting_names_test.py
+++ exercises/practice/tp-3-formatting-names/tp_3_formatting_names_test.py
@@ -1,8 +1,16 @@
 import unittest
 
-class Test(unittest.TestCase):
-    def test(self):
+from hello_world import (
+    hello,
+)
+
+# Tests adapted from `problem-specifications//canonical-data.json`
+
+
+class HelloWorldTest(unittest.TestCase):
+    def test_say_hi(self):
         self.assertEqual("Hello, World!", "Hello, World!")
+
 
 if __name__ == "__main__":
     unittest.main()
