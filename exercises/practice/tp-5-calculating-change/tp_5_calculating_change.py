932adb5233dd99607bfacfd1f46b3f14a4ef1c98
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2023-04-13 23:02:27 -0300

    added tp-5 calcualting change

---
+++ exercises/practice/tp-5-calculating-change/tp_5_calculating_change.py
@@ -0,0 +1,17 @@
+amount = input("Ingresar gasto")
+amount_payed = input("Dinero recibido")
+
+def calculate_integer_part_of_change(amount, amount_payed):
+    return 1
+
+
+def calculate_cents_part_of_change():
+    return 1
+
+println("Pesos")
+println(calculate_integer_part_of_change(amount, amount_payed))
+
+println("Centavos")
+println(calculate_cents_part_of_change(amount, amount_payed))
+
+
---
+++ exercises/practice/tp-5-calculating-change/tp_5_calculating_change_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+# Tests adapted from `problem-specifications//canonical-data.json`
+
+from tp_5_calculating_change import (
+    calculate_integer_part_of_change,
+    calculate_cents_part_of_change,
+)
+
+class HelloWorldTest(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(calculate_integer_part_of_change(100, 101.5), 1)
+
+    def test_2(self):
+        self.assertEqual(calculate_integer_part_of_change(100, 100.5), 0)
+
+    def test_1(self):
+        self.assertEqual(calculate_cents_part_of_change(100, 101.5), 50)
+
+    def test_2(self):
+        self.assertEqual(calculate_cents_part_of_change(100, 101), 0)
+
+if __name__ == "__main__":
+    unittest.main()
