a11774afc66b8d62e44e084b4fe1056f12beec40
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2023-04-13 22:52:33 -0300

    fixing test

--- exercises/practice/tp-4-sorting-countries/tp_4_sorting_countries_test.py
+++ exercises/practice/tp-4-sorting-countries/tp_4_sorting_countries_test.py
@@ -13,13 +13,13 @@ class HelloWorldTest(unittest.TestCase):
         self.assertEqual(x_is_before_than_y("a", "b"), "The result of a comes first in the dictionary than b is True.")
 
     def test_2(self):
-        self.assertEqual(x_is_before_than_y("b", "a"), "The result of a comes first in the dictionary than b is False.")
+        self.assertEqual(x_is_before_than_y("b", "a"), "The result of b comes first in the dictionary than a is False.")
 
     def test_3(self):
-        self.assertEqual(x_is_before_than_y("a", "b"), "The result of b comes first in the dictionary than a is False.")
+        self.assertEqual(y_is_before_than_x("a", "b"), "The result of b comes first in the dictionary than a is False.")
 
     def test_4(self):
-        self.assertEqual(x_is_before_than_y("b", "a"), "The result of b comes first in the dictionary than a is True.")
+        self.assertEqual(y_is_before_than_x("b", "a"), "The result of a comes first in the dictionary than b is True.")
 
 if __name__ == "__main__":
     unittest.main()
