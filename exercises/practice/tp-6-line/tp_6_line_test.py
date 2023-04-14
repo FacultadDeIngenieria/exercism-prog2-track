736d4f256e04b73c8dd73647f9b552c4ccacf1a3
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2023-04-13 23:27:30 -0300

    fix

--- exercises/practice/tp-6-line/tp_6_line_test.py
+++ exercises/practice/tp-6-line/tp_6_line_test.py
@@ -10,10 +10,10 @@ from tp_6_line import (
 
 class LineEquationTest(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(equation_to_string(1, 2), "Y = 1x  + 2")
+        self.assertEqual(equation_to_string(1, 2), "Y = 1x + 2")
 
     def test_2(self):
-        self.assertEqual(equation_to_string(-1, -2), "Y = -1x  + -2")
+        self.assertEqual(equation_to_string(-1, -2), "Y = -1x + -2")
 
     def test_3(self):
         self.assertEqual(point_to_string(1, 1, 0), "(0, 1)")
