d2538f15dfc542228aa1c489adcdc55c569f0d5c
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2023-04-13 23:09:38 -0300

    commented input

--- exercises/practice/tp-5-calculating-change/tp_5_calculating_change.py
+++ exercises/practice/tp-5-calculating-change/tp_5_calculating_change.py
@@ -1,5 +1,8 @@
-amount = input("Ingresar gasto")
-amount_payed = input("Dinero recibido")
+# If you want to run this in CLI, you can uncomment the commented lines,
+# If running on exercim, run it with the commented lines commented.
+
+# amount = input("Ingresar gasto")
+# amount_payed = input("Dinero recibido")
 
 def calculate_integer_part_of_change(amount, amount_payed):
     return 1
@@ -8,10 +11,10 @@ def calculate_integer_part_of_change(amount, amount_payed):
 def calculate_cents_part_of_change():
     return 1
 
-println("Pesos")
-println(calculate_integer_part_of_change(amount, amount_payed))
+# println("Pesos")
+# println(calculate_integer_part_of_change(amount, amount_payed))
 
-println("Centavos")
-println(calculate_cents_part_of_change(amount, amount_payed))
+# println("Centavos")
+# println(calculate_cents_part_of_change(amount, amount_payed))
 
 
