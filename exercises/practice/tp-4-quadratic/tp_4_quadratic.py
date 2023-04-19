f079e93e70a992e459298e4567b3464e50785dac
Matías Ponce <matias.ponce@mulesoft.com>
2023-04-19 20:29:21 -0300

    Added tp-4-quadratic python-doc

--- exercises/practice/tp-4-quadratic/tp_4_quadratic.py
+++ exercises/practice/tp-4-quadratic/tp_4_quadratic.py
@@ -2,16 +2,21 @@ import math
 
 
 def roots(a, b, c):
+    """Dado los parámetros (a, b, c) el método roots devolverá un String de la forma (r1, r2) o (r12) o ( ) según sea el caso de que tenga dos raíces, una raíz o ninguna."""
     return "ANSWER HERE"
 
 
 def value_y(a, b, c, x):
+    """Dado los parámetros (a, b, c, x) el método valueY devolverá el valor de Y para un valor de X que se le pasa como parámetro."""
     return "ANSWER HERE"
 
 
 def to_string(a, b, c):
+    """Dado los parámetros (a, b, c) el método toString que devolverá un String mostrando la ecuación como figura en el primer párrafo (Y = A * X2 + B * X + C)."""
     return "ANSWER HERE"
 
 
 def derivation(a, b):
+    """Dado los parámetros (a, b) el método derivation que devolverá un String mostrando la función lineal derivada.
+"""
     return "ANSWER HERE"
